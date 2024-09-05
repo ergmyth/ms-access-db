9dced6e564f7a2fde43e232b183e2a252f40d842
ARFatkullin <ARFatkullin@ufa.beeline.ru>
2024-09-05 13:45:06 +0500

    f

--- start.sh
+++ start.sh
@@ -3,8 +3,10 @@
 
 
 # Kill previous launched executables
-pkill -u root java
-pkill -u root chrome
+pkill -9 java
+pkill -9 chrome
+pkill -9 chromedriver
+Pkill -9 chromium
 rm -rf ../../tmp/undetected_chrome_driver*
 rm -rf ../../tmp/snap-private-tmp/snap.chromium/tmp/undetected_chrome_driver*
 
